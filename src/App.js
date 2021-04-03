bb77249125351afe0253a344206290ee1c4352c1
yoho4613 <79840170+yoho4613@users.noreply.github.com>
2021-04-04 01:59:56 +1300

    Navbar Completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <div className="App">
-      <h2>hi</h2>
+      <Navbar />
     </div>
   );
 }
